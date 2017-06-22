cfc41ca0d49598830f769188f05931af556dc6b6
Cody May <codymay@mail.weber.edu>
2017-06-21 19:00:35 -0600

    assignment completed

--- dbConnect.php
+++ dbConnect.php
@@ -1,11 +1,11 @@
 <?php
 
-// database functions ************************************************
 
-function fConnectToDatabase() {
-   $db = new PDO("mysql:host=localhost;dbname=testdb', 'username', 'password');
-   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-   return $db;
-}
+function fConnectToDatabase(){
+	$db= new PDO('mysql:localhost:3306=mysql;dbname=dvd', 'root', '##DerpDerp90');
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+	echo 'ok';
+	return $db;
+	}
 ?>
--- mySQL.php
+++ mySQL.php
@@ -1,18 +1,56 @@
 <?php
 // database functions ************************************************
-
-function fInsertToDatabase() {
+function fInsertToDatabase($myDB, $asin, $title, $price) {
   $sql = "INSERT INTO dvdtitles (asin, title, price) VALUES ('$asin', '$title', $price)";
-  // TODO: Fill in the rest of the fuction
+  $stmt= $myDB->query($sql);
+  
+  
+  
+}
+function fDeleteFromDatabase($myDB, $asin) {
+  $sql = "DELETE FROM dvdtitles WHERE asin = '$asin'";
+
+  $stmt = $myDB->query($sql);
+ 
+  // TODO: Fill in the rest of the function
+}
+function fListFromDatabase($myDB) {
+	echo "ok ". PHP_EOL;
+    $sql = 'SELECT * from dvdtitles;';
+    $stmt = $myDB->query($sql);
+  
+  
+    while($row =$stmt->fetch()) {
+		//$title= $row['title']; 
+		print_r ($row['title']);
+		$imloc='http://images.amazon.com/images/P/' . $row['asin'] . '.01.MZZZZZZZ.jpg';
+		//echo $imloc;
+		$imageData = base64_encode(file_get_contents($imloc));
+		echo '<img src="data:image/jpeg;base64,'.$imageData.'">';
+		
+	}
 }
 
-function fDeleteFromDatabase() {
-  $sql = "DELETE FROM tblCustomers WHERE CustID=$deleteID";
-  // TODO: Fill in the rest of the fuction
+function listActors($myDB){
+	$sql='SELECT * from dvdactors';
+	$stmt = $myDB->query($sql);
+	 while($row =$stmt->fetch()) {
+		
+		print_r ($row['fname'] . " " . $row['lname'] . PHP_EOL);
+		
+		
+	 }
+	
 }
+function AddActor($myDB, $fname, $lname){
+	 $sql = "INSERT INTO dvdactors (fname, lname) VALUES ('$fname', '$lname')";
+	 $stmt= $myDB->query($sql);
+  
+	
+}
+function removeActor($myDB, $id){
+	$sql = "DELETE FROM dvdactors WHERE id = '$id'";
 
-function fListFromDatabase() {
-  $sql = 'SELECT custID, nameF, nameL FROM tblCustomers ORDER BY CustID';
-  // TODO: Fill in the rest of the fuction
+	$stmt = $myDB->query($sql);
+	
 }
-?>
